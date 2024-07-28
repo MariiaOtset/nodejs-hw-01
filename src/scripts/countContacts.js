a5e4d278cbbda1ff8baf8666daee72ae0790c7b7
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-07-28 15:50:23 +0100

    6 scripts done

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  const data = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+  return data.length;
+};
 
 console.log(await countContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = JSON.parse(await fs.readFile(PATH_DB, 'utf-8'));
+    if (data.length > 0) {
+      data.pop();
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(data));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 removeLastContact();
+
+//npm run remove-last
